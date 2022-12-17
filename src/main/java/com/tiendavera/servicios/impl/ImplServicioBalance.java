adc187d2a030f6e016d94ac7e2fce2660dde2a56
Hugo Cuba Juarez <89067544+Hugocj@users.noreply.github.com>
2022-12-16 20:32:19 -0500

    Update ImplServicioBalance.java
    cambio en linea 106

--- src/main/java/com/tiendavera/servicios/impl/ImplServicioBalance.java
+++ src/main/java/com/tiendavera/servicios/impl/ImplServicioBalance.java
@@ -109,6 +109,7 @@ public class ImplServicioBalance implements ServicioBalance {
 
     @Override
     public void deleteBalance(Integer idBalance) {
+        System.out.println("tienda vera");
         this.repositorioBalance.deleteById(idBalance);
     }
 }
