8a776f80262562ad6deb55701cabc4db8a90ba2d
Hugo Vera Sánchez <65966143+Tryop-X@users.noreply.github.com>
2022-12-16 20:54:38 -0500

    Agregando un array de prueba

--- src/main/java/com/tiendavera/servicios/impl/ImplServicioBalance.java
+++ src/main/java/com/tiendavera/servicios/impl/ImplServicioBalance.java
@@ -110,6 +110,7 @@ public class ImplServicioBalance implements ServicioBalance {
     @Override
     public void deleteBalance(Integer idBalance) {
         System.out.println("tienda vera");
+        System.out.println("Array de pruueba")
         this.repositorioBalance.deleteById(idBalance);
     }
 }
